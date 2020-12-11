520e6c4d6bb9c224f8809795d87f1d6eca8a71b7
Guti <g.a.a612.ag@gmail.com>
2020-12-11 10:48:33 +0100

    ..
    ..

--- Roman/src/main.js
+++ Roman/src/main.js
@@ -1,4 +1,4 @@
-function arabicToRoman(number){
+const arabicToRoman = (number) =>{
   let roman = "";
   const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
   let a;
@@ -17,4 +17,7 @@ function arabicToRoman(number){
   }
 
   return roman;
+}
+module.exports = {
+arabicToRoman:arabicToRoman
 }
